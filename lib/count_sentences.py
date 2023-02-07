15d71edd4c030b8f73cef06ddd9ec36a6b85253f
Parker Parham <parkerparham@gmail.com>
2023-02-07 15:31:07 -0500

    Adds function to test if value ends in a period

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+
+  def __init__(self, value = ""):
+    self._value = value
+    
+  def get_value(self):
+    return self._value
+
+  def set_value(self, stringVal):
+    if (type(stringVal) == str):
+      self._value = stringVal
+    else:
+      print("The value must be a string.")
+
+  value = property(get_value, set_value)
+
+  def is_sentence(self):
+    return self._value.endswith(".")
